28a126820b2c61f9e89eee40ad38f5b0a36c4bdb
smitacha <smitachavare94@gmail.com>
2017-07-07 12:12:01 +0530

    user
    usdidfhj

---
+++ User.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="User.aspx.cs" Inherits="User" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td><asp:Label runat="server" >Name</asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtName"></asp:TextBox></td>
+                <asp:RequiredFieldValidator runat="server" ControlToValidate="txtName" ErrorMessage="Please Provide Name"> </asp:RequiredFieldValidator>
+                <%--<asp:RangeValidator runat="server" ControlToValidate="txtName" ></asp:RangeValidator>--%>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" >Mobile No</asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtMob"></asp:TextBox></td>
+                <%-- <asp:RequiredFieldValidator runat="server" ControlToValidate="txtMob" ErrorMessage="Please Provide Mobile No"> </asp:RequiredFieldValidator>--%>
+                <asp:RangeValidator ControlToValidate="txtMob" runat="server" ></asp:RangeValidator>
+            </tr>
+            <tr>
+                <td><asp:Label runat="server" >Join Date</asp:Label></td>
+                <td><asp:TextBox ID="txtDate" runat="server"></asp:TextBox></td>
+                <%--<asp:RequiredFieldValidator runat="server" ControlToValidate="txtDate" ErrorMessage="Please Provide Join Date"> </asp:RequiredFieldValidator>--%>
+            </tr>
+
+             <tr>
+                <td><asp:Label runat="server" >Country</asp:Label></td>
+                <td>
+                    <asp:DropDownList ID="ddlCountry" runat="server" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+                 
+            </tr>
+            
+              <tr>
+                <td><asp:Label runat="server" >State</asp:Label></td>
+                <td>
+                    <asp:DropDownList ID="ddlState" runat="server">
+                    </asp:DropDownList>
+                </td>
+                 
+            </tr>
+
+
+            <tr>
+                <td><asp:Label runat="server" >City</asp:Label></td>
+                <td>
+                    <asp:DropDownList ID="ddlCity" runat="server">
+                    </asp:DropDownList>
+                </td>
+                <%-- <asp:RequiredFieldValidator runat="server" ControlToValidate="ddlCity" ErrorMessage="Please Provide City"> </asp:RequiredFieldValidator>--%>
+            </tr>
+             <tr>
+                <td colspan="2"><asp:Button runat="server" text="Save" ForeColor="#0066ff" OnClick="Unnamed11_Click"/></td>
+                 
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button1" runat="server"  ForeColor="#0066ff"  Text="Save Tarun" OnClick="Button1_Click" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+    
+</body>
+</html>
---
+++ User.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class User : System.Web.UI.Page
+{
+    string cs = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DisplayCity();
+        dispCountry();
+        dispState();
+    }
+    protected void Unnamed11_Click(object sender, EventArgs e)
+    {
+        proc();
+
+    }
+
+    private void DisplayCity()
+    {
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select CityId,CityName from City", con);
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        ddlCity.DataTextField = "CityName";
+        ddlCity.DataValueField = "CityId";
+        ddlCity.DataSource = dt;
+       
+        ddlCity.DataBind();
+        con.Close();
+    }
+
+    private void dispCountry()
+    {
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("SELECT CounId,Cname FROM Country ", con);
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        ddlCountry.DataTextField = "Cname";
+        ddlCountry.DataValueField = "CounId";
+        ddlCountry.DataSource = dt;
+
+        ddlCountry.DataBind();
+        con.Close();
+    }
+
+    private void dispState()
+    {
+        //SqlConnection con = new SqlConnection(cs);
+        //con.Open();
+        //SqlCommand cmd = new SqlCommand("SELECT StId,StateName FROM CState where CounId="+ddlCountry.SelectedValue, con);
+        //DataTable dt = new DataTable();
+        //SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //da.Fill(dt);
+        //ddlState.DataTextField = "StateName";
+        //ddlState.DataValueField = "StId";
+        //ddlState.DataSource = dt;
+
+        //ddlState.DataBind();
+        //con.Close();
+    }
+
+
+    private void proc()
+    {
+        
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("insDetails", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@name", txtName.Text);
+        cmd.Parameters.AddWithValue("@mobile", txtMob.Text);
+        cmd.Parameters.AddWithValue("@city", ddlCity.SelectedValue);
+        cmd.Parameters.AddWithValue("@date", txtDate.Text);
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        proc();
+    }
+    protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("SELECT StId,StateName FROM CState where CounId=" + ddlCountry.SelectedValue, con);
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        ddlState.DataTextField = "StateName";
+        ddlState.DataValueField = "StId";
+        ddlState.DataSource = dt;
+
+        ddlState.DataBind();
+        con.Close();
+    }
+}
